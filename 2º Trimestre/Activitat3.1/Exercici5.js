1562d8bf37cfc12677b230cd756b0bb4ff6bacc8
dd0minguez <ddominguez@nigul.cide.es>
2023-12-08 19:16:17 +0100

    Exercici5

--- 2º Trimestre/Activitat3.1/Exercici5.js
+++ 2º Trimestre/Activitat3.1/Exercici5.js
@@ -1,5 +1,13 @@
-var aleatorio_aleatorio = Math. random()
-
-function random(min, max) {
-    return Math.floor((Math.random() * (max - min + 1)) + min);
+  Alerta = setInterval(sensor, 1000)
+  function valorSensor(max) {
+    return Math.floor(Math.random() * max);
+  }
+function sensor(){
+    Random = valorSensor(45);
+    if(Random > 40){
+        console.log("Temperatura alta, llamar a los bomberos")
+        clearInterval(Alerta)
+    }
+    console.log(Random)
 }
+  
