7bcd503fb94a18c6b0905e1cb2353385699513fc
Denis Domínguez <denisdominguez@Ordenador-portatil-de-Denis.local>
2024-02-29 13:33:47 +0100

    listo

--- myapp/src/todo/TodoList.tsx
+++ myapp/src/todo/TodoList.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import TodoItem from "./TodoItem";
 import {Button} from "react-bootstrap";
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 function TodoList(){
     const [tasks, setTask] = React.useState(["Tasca1", "Tasca2"]);
 
     function addTask(texto: string){
         setTask(currentTasks => [...currentTasks, texto]);
+        <InputGroup>
+            <Form.Control
+                placeholder="Recipient's username"
+                aria-label="Recipient's username with two button addons"
+        />
+        <Button variant="outline-secondary">Quitar</Button>
+    </InputGroup>
     }
     return(
         <div>
