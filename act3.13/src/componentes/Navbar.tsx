8a16a21c45f631cb0bd110d62b1bed2b0294190d
Denis Domínguez <denisdominguez@Ordenador-portatil-de-Denis.local>
2024-03-12 18:10:29 +0100

    Preparado

--- act3.13/src/componentes/Navbar.tsx
+++ act3.13/src/componentes/Navbar.tsx
@@ -14,11 +14,11 @@ return (
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
             <Nav className="justify-content-center mx-4" style={{ maxHeight: '100px' }} navbarScroll>
-                <Nav.Link href="/">Inicio</Nav.Link>
-                <Nav.Link href="PuntoInteres">Puntos de Interés</Nav.Link>
-                <Nav.Link href="Restaurantes">Restaurantes</Nav.Link>
-                <Nav.Link href="Hoteles">Hoteles</Nav.Link>
-                <Nav.Link href="Actividades">Actividades</Nav.Link>
+                <Nav.Link as={NavLink} href="/">Inicio</Nav.Link>
+                <Nav.Link as={NavLink} href="PuntoInteres">Puntos de Interés</Nav.Link>
+                <Nav.Link as={NavLink} href="Restaurantes">Restaurantes</Nav.Link>
+                <Nav.Link as={NavLink} href="Hoteles">Hoteles</Nav.Link>
+                <Nav.Link as={NavLink} href="Actividades">Actividades</Nav.Link>
             </Nav>
             {/* <Form className="d-flex">
                 <Form.Control type="search" placeholder="Busca..." className="me-2" aria-label="Search"/>
