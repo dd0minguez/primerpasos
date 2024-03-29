efc4e4882f2f294252d3f0816adb2c8b9e73e789
Denis Domínguez <denisdominguez@Ordenador-portatil-de-Denis.local>
2024-03-29 11:01:15 +0100

    Problemas con el build

--- act3.14/src/App.tsx
+++ act3.14/src/App.tsx
@@ -1,17 +1,14 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Inicio from './paginas/Inicio'
-import RazaInfo from './paginas/GatoInfo';
-import RazaPInfo from './paginas/PerrosInfo';
+
 
 function App() {
   return (
     <>
        <BrowserRouter >
         <Routes>
-          <Route index element={<Inicio />} />
-          <Route path='RazaGato' element={<RazaInfo id={""}/>} />
-          <Route path='RazaPerro' element={<RazaPInfo id={""}/>} />
+          <Route path='/' index element={<Inicio />} />
         </Routes>
       </BrowserRouter>
     </>
